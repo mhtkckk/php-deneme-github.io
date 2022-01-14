b6bbf4b4a78a11a55f6a49101888749b5b3d073b
Mehmet KÜÇÜK <93263915+mhtkckk@users.noreply.github.com>
2022-01-14 22:49:13 +0300

    Add files via upload

---
+++ php.index.php
@@ -0,0 +1,18 @@
+<?php
+$a=20;
+$b=5;
+$toplam=$a+$b;
+$fark=$a-$b;
+$carpim=$a*$b;
+$bolum=$a/$b;
+$kalan=$a%$b;
+echo "TOPLAM".$toplam;
+echo "<br>";
+echo "FARK".$fark;
+echo "<br>";
+echo "ÇARPIM".$carpim;
+echo "<br>";
+echo "BÖLÜM".$bolum;
+echo "<br>";
+echo "KALAN".$kalan;
+?>
